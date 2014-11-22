59517996677c63fa4d938647df81db766db78f94
Alex Reynolds <cam00674@myport.ac.uk>
2014-11-22 15:24:52 +0000

    Sort out conflicting issues from Gavin's changes

--- Client/main.js
+++ Client/main.js
@@ -1,5 +1,11 @@
-function initialize() {
+var map;
 
+function initialize() {
+    SetupMap();
+    PlotHeatmaps();
+}
+
+function SetupMap() {
     var mapOptions = {
         center: {
             lat: 54.523610,
@@ -8,7 +14,7 @@ function initialize() {
         zoom: 6
     };
     
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);    
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
