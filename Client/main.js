9d1453ba4bf5aeddb982a18f3dfdec1c8c54d606
Gavin Owen <gavinowen16@gmail.com>
2014-11-22 15:09:19 +0000

    add getCountries

--- Client/main.js
+++ Client/main.js
@@ -1,5 +1,5 @@
 function initialize() {
-    
+
     var mapOptions = {
         center: {
             lat: 54.523610,
@@ -12,3 +12,34 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+function getCountries() {
+
+    var jsonContents, headingsArray, dataArray, countriesArray = [];
+
+    $.ajax({
+      url: 'Data/Country_db.json',
+      async: false,
+      dataType: 'json',
+      success: function (response) {
+            jsonContents = response;
+        }
+    });
+
+    headingsArray = jsonContents.meta.view.columns;
+    dataArray = jsonContents.data;
+
+    for (var i = dataArray.length - 1; i >= 0; i--) {
+
+        countriesArray.push({});
+
+        for (var b = headingsArray.length - 1; b >= 0; b--) {
+
+            countriesArray[countriesArray.length-1][headingsArray[b].name] = dataArray[i][b];
+
+        };
+    };
+
+    return countriesArray;
+
+}
