5f8c59a4c165d21a559c2a3029885269c59a305a
Alex Reynolds <cam00674@myport.ac.uk>
2014-11-22 16:39:37 +0000

    move data and plot points

--- Client/csvparser.js
+++ Client/csvparser.js
@@ -3,9 +3,9 @@ function ParseCSVs() {
 	$(data).each(function(index,item){
 		if (!item.populationData) {
 			$.ajax(
-				'/data/cache/'+ item['Alpha-3 code'] +'_SP_POP_TOTL.csv',{
+				'/Data/cache/WORLDBANK-'+ item['Alpha-3 code'] +'_SP_POP_TOTL.csv',{
 					async: false,
-					success: function() {
+					success: function(r) {
 						item.populationData = $.csv.toObjects(r);						
 					}
 				});
